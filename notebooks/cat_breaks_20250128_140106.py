e93e0056a9652cb1adcc19a9bafc863fbbe48110
Minase Taye <minasegirma5@gmail.com>
2025-01-28 14:52:21 +0300

    rfm and woe

---
+++ notebooks/cat_breaks_20250128_140106.py
@@ -0,0 +1,5 @@
+breaks_list={
+'ChannelId': ['ChannelId_5%,%ChannelId_2','ChannelId_3%,%ChannelId_1'], 
+'ProductCategory': ['data_bundles%,%movies%,%other%,%ticket%,%tv%,%airtime','financial_services%,%utility_bill%,%transport'], 
+'ProductId': ['ProductId_1%,%ProductId_24%,%ProductId_23%,%ProductId_4%,%ProductId_8%,%ProductId_20%,%ProductId_2%,%ProductId_27%,%ProductId_19%,%ProductId_14%,%ProductId_7%,%ProductId_12%,%ProductId_11%,%ProductId_16%,%ProductId_6','ProductId_10','ProductId_3%,%ProductId_21','ProductId_15%,%ProductId_13%,%ProductId_22%,%ProductId_5%,%ProductId_9'], 
+'ProviderId': ['ProviderId_2%,%ProviderId_6%,%ProviderId_4','ProviderId_5','ProviderId_1%,%ProviderId_3']}
